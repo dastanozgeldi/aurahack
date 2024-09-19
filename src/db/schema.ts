1029d3d01b45d5eb5e7c53a45e4f3c9bf4280a7f
Dastan Ozgeldi <ozgdastan@gmail.com>
2024-09-19 23:32:24 +0500

    feat: dump somewhat acceptable database schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,27 +1,118 @@
-import { integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+import { relations } from "drizzle-orm";
+import {
+  integer,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable('users_table', {
-  id: serial('id').primaryKey(),
-  name: text('name').notNull(),
-  age: integer('age').notNull(),
-  email: text('email').notNull().unique(),
-});
+export const organizersTable = pgTable("organizers_table", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
+  email: text("email").notNull().unique(),
+  phone: text("phone").notNull(),
 
-export const postsTable = pgTable('posts_table', {
-  id: serial('id').primaryKey(),
-  title: text('title').notNull(),
-  content: text('content').notNull(),
-  userId: integer('user_id')
-    .notNull()
-    .references(() => usersTable.id, { onDelete: 'cascade' }),
-  createdAt: timestamp('created_at').notNull().defaultNow(),
-  updatedAt: timestamp('updated_at')
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
     .notNull()
     .$onUpdate(() => new Date()),
 });
 
-export type InsertUser = typeof usersTable.$inferInsert;
-export type SelectUser = typeof usersTable.$inferSelect;
+export const organizersRelations = relations(organizersTable, ({ many }) => ({
+  hackathons: many(hackathonsTable),
+}));
 
-export type InsertPost = typeof postsTable.$inferInsert;
-export type SelectPost = typeof postsTable.$inferSelect;
+export const hackathonsTable = pgTable("hackathons_table", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
+  description: text("description").notNull(),
+  organizerId: integer("organizer_id")
+    .references(() => organizersTable.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .$onUpdate(() => new Date()),
+});
+
+export const hackathonsRelations = relations(
+  hackathonsTable,
+  ({ one, many }) => ({
+    organizer: one(organizersTable, {
+      fields: [hackathonsTable.organizerId],
+      references: [organizersTable.id],
+    }),
+    teams: many(teamsTable),
+    projects: many(projectsTable),
+  }),
+);
+
+export const teamsTable = pgTable("teams_table", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
+  hackathonId: integer("hackathon_id"),
+
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .$onUpdate(() => new Date()),
+});
+
+export const teamsRelations = relations(teamsTable, ({ one, many }) => ({
+  hackathon: one(hackathonsTable, {
+    fields: [teamsTable.hackathonId],
+    references: [hackathonsTable.id],
+  }),
+  members: many(profilesTable),
+}));
+
+export const profilesTable = pgTable("profiles_table", {
+  userId: text("user_id").primaryKey(),
+  name: text("name").notNull(),
+  username: varchar("username", { length: 20 }).unique(),
+
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .$onUpdate(() => new Date()),
+});
+
+export const profilesRelations = relations(profilesTable, ({ one }) => ({
+  user: one(teamsTable, {
+    fields: [profilesTable.userId],
+    references: [teamsTable.id],
+  }),
+}));
+
+export const projectsTable = pgTable("projects_table", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
+  githubUrl: text("github_url").notNull(),
+  teamId: integer("team_id")
+    .references(() => teamsTable.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  hackathonId: integer("hackathon_id"),
+
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .$onUpdate(() => new Date()),
+});
+
+export const projectsRelations = relations(projectsTable, ({ one }) => ({
+  team: one(teamsTable, {
+    fields: [projectsTable.teamId],
+    references: [teamsTable.id],
+  }),
+  hackathon: one(hackathonsTable, {
+    fields: [projectsTable.hackathonId],
+    references: [hackathonsTable.id],
+  }),
+}));
