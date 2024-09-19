f5864aaeea8a9cdb8c5cb8bd2d52fc49cf9bc87f
Dastan Ozgeldi <ozgdastan@gmail.com>
2024-09-20 00:45:42 +0500

    feat: command menu setup

---
+++ src/components/command-menu.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import React from "react";
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "./ui/command";
+import { Button } from "./ui/button";
+import { Command } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+export function CommandMenu() {
+  const router = useRouter();
+  const [open, setOpen] = React.useState(false);
+
+  React.useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+
+  const groups = [
+    {
+      name: "Compete",
+      links: [
+        { name: "Hackathons", href: "/hackathons" },
+        { name: "Teams", href: "/teams" },
+        { name: "Projects", href: "/projects" },
+      ],
+    },
+    {
+      name: "Fun",
+      links: [{ name: "Calculate your winning chances", href: "/win-chance" }],
+    },
+  ];
+
+  return (
+    <>
+      <Button variant="outline" className="px-3" onClick={() => setOpen(true)}>
+        <Command className="h-4 w-4" />
+      </Button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          {groups.map((group) => (
+            <CommandGroup key={group.name} heading={group.name}>
+              {group.links.map((link) => (
+                <CommandItem
+                  key={link.name}
+                  onSelect={() => {
+                    router.push(link.href);
+                    setOpen(false);
+                  }}
+                >
+                  {link.name}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          ))}
+        </CommandList>
+      </CommandDialog>
+    </>
+  );
+}
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,7 +1,7 @@
-import { cn } from "@/lib/utils";
 import Link from "next/link";
-import { Button, buttonVariants } from "./ui/button";
 import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
+import { Button } from "./ui/button";
+import { CommandMenu } from "./command-menu";
 
 export const Nav = () => (
   <nav className="flex items-center justify-between">
@@ -9,16 +9,7 @@ export const Nav = () => (
       <h1 className="text-2xl font-extrabold sm:text-3xl">aurahack.</h1>
     </Link>
 
-    <div className="flex items-center gap-3 sm:gap-6">
-      <Link
-        className={cn(
-          buttonVariants({ variant: "link" }),
-          "px-0 text-base sm:text-lg"
-        )}
-        href="/win-chance"
-      >
-        win chance
-      </Link>
+    <div className="flex items-center gap-3">
       <SignedOut>
         <SignInButton>
           <Button>sign in</Button>
@@ -27,6 +18,8 @@ export const Nav = () => (
       <SignedIn>
         <UserButton />
       </SignedIn>
+
+      <CommandMenu />
     </div>
   </nav>
 );
